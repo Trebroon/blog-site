863d805c3f6e62c0a46527f6db65ff244bdc1bc7
Trebroon <leipold.norbert@gmail.com>
2022-03-17 18:58:01 +0100

    connected react to backend in dashboard

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import Axios from 'axios'
 
 // styles
 import './Dashboard.css'
@@ -6,9 +7,22 @@ import './Dashboard.css'
 export default function Dashboard() {
   const [blogs, setBlogs] = useState([])
 
+  useEffect(() => {
+    Axios.get('http://localhost:5000/').then((response) => {
+      setBlogs(response.data)
+    })
+  }, [])
+
   return (
     <div>
-
+      {blogs.map((blog) => {
+        return (
+          <div key={blog._id}>
+            <h3>{blog.title}</h3>
+            <p>{blog.text}</p>
+          </div>
+        )
+      })}
     </div>
   )
 }
--- server/server.js
+++ server/server.js
@@ -10,11 +10,10 @@ const blogRoutes = require('./routes/blogRoutes')
 const port = process.env.PORT || 5000
 
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 connectDB();
-
 
 app.use('/', blogRoutes)
 
