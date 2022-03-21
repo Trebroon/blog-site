0df69f8f5f743683a133550375f4d506b98ec9fb
Trebroon <leipold.norbert@gmail.com>
2022-03-21 13:54:42 +0100

    backend: added mongoSanizize

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoSanitize = require('express-mongo-sanitize');
 const cors = require('cors')
 const dotenv = require('dotenv').config()
 const connectDB = require('./config/db')
@@ -6,16 +7,16 @@ const connectDB = require('./config/db')
 // routes
 const blogRoutes = require('./routes/blogRoutes')
 
-
-const port = process.env.PORT || 5000
-
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(mongoSanitize());
 
 connectDB();
 
 app.use('/api', blogRoutes)
+
+const port = process.env.PORT || 5000
 
 app.listen(port, () => {
   console.log(`Serving on port ${port}`);
