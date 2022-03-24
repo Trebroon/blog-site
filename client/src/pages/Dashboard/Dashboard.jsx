14c910c7294523842178c94206fa1c99aa76ea44
Trebroon <leipold.norbert@gmail.com>
2022-03-24 12:39:53 +0100

    frontend: added loading articles div to dashboard component when data is loading

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -7,19 +7,25 @@ import BlogCard from '../../components/BlogCard'
 
 export default function Dashboard() {
   const [blogs, setBlogs] = useState([])
+  const [isPending, setIsPending] = useState(false)
 
   useEffect(() => {
+    setIsPending(true)
     axios.get('/api').then((response) => {
       setBlogs(response.data)
     })
+    setIsPending(false)
   }, [])
 
   return (
     <div className="dashboard">
       <h2>Most recent articles</h2>
-      <div className='blogs-grid'>
+      {isPending && <div>Loading articles...</div>}
+      {!isPending && (
+        <div className='blogs-grid'>
         {blogs.map((blog) => <BlogCard blog={blog} key={blog._id} />)}
-      </div>
+        </div>
+      )}
     </div>
   )
 }
